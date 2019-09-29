66d8d8611347e6359cb3a0e00e56342816e2e367
Cody Dupuis <ctdupuis7@gmail.com>
2019-09-29 01:45:20 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,7 +10,7 @@ class EmailParser
   end
   
   def parse(csv_emails)
-    @csv_emails.split.collect do |address|
+    csv_emails.split.collect do |address|
       address.split(",")
     end
     .flatten.uniq
